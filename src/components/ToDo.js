5abb792debebd66d1efabc73c65e68240ff9e7de
Apdet <deterding1.a@gmail.com>
2018-05-06 22:37:06 -0500

    event

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,9 +3,12 @@ import React, { Component } from 'react';
 class ToDo extends Component {
   render() {
      return (
-       <li> A todo will go here </li>
+       <li>
+       <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+         <span>{ this.props.description }</span>
+       </li>
      );
    }
-}
+ }
 
 export default ToDo;

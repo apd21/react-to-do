a2d391201dac64d332ab38047476bcec06ca66b7
Apdet <deterding1.a@gmail.com>
2018-05-07 14:34:09 -0500

    delete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,77 @@ import React, { Component } from 'react';
 import './App.css';
 import ToDo from './components/ToDo.js';
 
+export default class Todos extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { todos: [], text: '' };
+    this.removeItem = this.removeItem.bind(this)
+  }
+
+  addTodo(e) {
+    e.preventDefault();
+    this.setState({ todos: [ this.state.text, ...this.state.todos ], text: '' });
+  }
+
+  updateValue(e) {
+    this.setState({ text: [e.target.value]})
+  }
+  removeItem(index) {
+    const todos = this.state.todos.filter((todo, todoIndex) => {
+      return todoIndex !== index
+    })
+    this.setState({ todos })
+  }
+  render() {
+    return(
+      <div>
+        <form onSubmit = {(e) => this.addTodo(e)}>
+          <input
+            placeholder="Add Todo"
+            value={this.state.text}
+            onChange={(e) => {this.updateValue(e)}}
+            />
+          <button type="submit">Add Todo</button>
+        </form>
+        <TodoList todos={this.state.todos} removeItem={this.removeItem} />
+      </div>
+    );
+  }
+}
+
+class TodoList extends Component {
+  render() {
+    return(
+      <ul>
+        { this.props.todos.map((todo, index) => {
+          return <li onClick={(e) => { this.props.removeItem(index)}} key={todo}>{ todo }</li>
+        })}
+      </ul>
+    );
+  }
+}
+
+removeItem(index) {
+  this.setState({
+    todos: this.state.todos.filter(_, i) >= i !== index
+  })
+}
+
+render() {
+return <ul>
+        {this.props.items.map((item, i) => {
+          return <li key={item}>
+                    {item}
+                    <a href = '#' onClick = {this.handleDelete}>
+                    [x]
+                    </a>
+                    </li>
+              }) }
+              </ul>;
+        }
+      }
+
+
 
 class App extends Component {
   constructor(props) {

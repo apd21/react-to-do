edf49b734da4eaef04f064e196e28b83d696bea8
Apdet <deterding1.a@gmail.com>
2018-05-15 18:51:14 -0500

    "new syntax"

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -9,12 +9,11 @@ class ToDo extends Component {
        <li>
        <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
          <span>{ this.props.description }</span>
-       </li>
-       <ul>
-           { this.props.todos.map((todo) => {
-               return <li onClick={() => { this.removeItem(todo)}} key={todo}>{ todo }</li>
-           })}
-       </ul>
+         { this.props.todos.map((todo) => {
+             return <li onClick={() => { this.removeItem(todo)}} key={todo}>{ todo }</li>
+         })}
+      </li>
+       
      );
    }
  }

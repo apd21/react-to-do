eb94b9cae21e736a194d5f04f992c45e0b691120
Apdet <deterding1.a@gmail.com>
2018-05-14 22:01:37 -0500

    "added onclick event"

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 
-
 class ToDo extends Component {
+  removeItem(e) {
+      this.props.removeTodo(item);
+  }
   render() {
      return (
        <li>
        <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
          <span>{ this.props.description }</span>
        </li>
+       <ul>
+           { this.props.todos.map((todo) => {
+               return <li onClick={() => { this.removeItem(todo)}} key={todo}>{ todo }</li>
+           })}
+       </ul>
      );
    }
  }

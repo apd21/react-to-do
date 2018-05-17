faeedd6eec8e0c9626febd07a130d7beec8f813d
Apdet <deterding1.a@gmail.com>
2018-05-16 21:22:24 -0500

    "added handle remove props"

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 
 class ToDo extends Component {
-  removeItem(e) {
-      this.props.removeTodo(item);
+
   }
   render() {
      return (
        <li>
+       {this.props.todos.map((todo, i) => {
+         return <ul key = {item} >
+                    {item}
+                    <a href = '#' onClick ={this.props.handleRemove.bind(null, todo) }>
+                    [x]
+                    </a>
+                    </ul>
+       }}
        <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
          <span>{ this.props.description }</span>
+         <button onClick = {props.deleteEvent} > Delete </button>
          { this.props.todos.map((todo) => {
-             return <li onClick={() => { this.removeItem(todo)}} key={todo}>{ todo }</li>
+
          })}
       </li>
-       
+
      );
    }
  }

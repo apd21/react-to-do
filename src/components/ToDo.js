586fd82d7eb1e4bf36f252b8ac00d40970ff1d02
Apdet <deterding1.a@gmail.com>
2018-05-18 15:50:16 -0500

    "changed item to todo"

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
  class ToDo extends Component {
+   removeItem(e) {
+        this.props.removeTodo(todo);
+    }
    render() {
      return (
        <li>
          <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
          <span>{this.props.description}</span>
-         { this.props.todos.map((todo) => {
-                    return <li onClick={() => { this.removeItem(todo)}} key={todo}>{ todo }</li>
+         { this.props.todo.map((todo) => {
+                    return <li onClick={() => { this.removeTodo(todo)}} key={todo}>{ todo }</li>
                 })}
        </li>
      );

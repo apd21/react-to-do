7fe88bb13c10edc9b03d1a1e774683ffeecedc3f
Apdet <deterding1.a@gmail.com>
2018-05-17 21:00:23 -0500

    "erased onclick insided todo.js"

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -6,20 +6,8 @@ class ToDo extends Component {
   render() {
      return (
        <li>
-       {this.props.todos.map((todo, i) => {
-         return <ul key = {item} >
-                    {item}
-                    <a href = '#' onClick ={this.props.handleRemove.bind(null, todo) }>
-                    [x]
-                    </a>
-                    </ul>
-       }}
        <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
          <span>{ this.props.description }</span>
-         <button onClick = {props.deleteEvent} > Delete </button>
-         { this.props.todos.map((todo) => {
-
-         })}
       </li>
 
      );

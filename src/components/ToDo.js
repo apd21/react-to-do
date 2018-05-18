c885fb21669e4c58f8560353b10c42b66a558bfb
Apdet <deterding1.a@gmail.com>
2018-05-18 14:37:23 -0500

    "added props and onclick"

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 
-class ToDo extends Component {
-
-  }
-  render() {
+ class ToDo extends Component {
+   render() {
      return (
        <li>
-       <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
-         <span>{ this.props.description }</span>
-      </li>
-
+         <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+         <span>{this.props.description}</span>
+         { this.props.todos.map((todo) => {
+                    return <li onClick={() => { this.removeItem(todo)}} key={todo}>{ todo }</li>
+                })}
+       </li>
      );
    }
  }
 
-export default ToDo;
+ export default ToDo;

81da36975469688b6507ee37bafd7921e4784579
Apdet <deterding1.a@gmail.com>
2018-05-18 17:23:40 -0500

    "changed syntax'

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 
  class ToDo extends Component {
    removeItem(e) {
-        this.props.removeTodo(todo);
+        this.props.removeTodo(ToDo);
     }
    render() {
      return (
        <li>
          <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
          <span>{this.props.description}</span>
-         { this.props.todo.map((todo) => {
-                    return <li onClick={() => { this.removeTodo(todo)}} key={todo}>{ todo }</li>
+         { this.props.ToDo.map((ToDo) => {
+                    return <li onClick={() => { this.removeTodo(ToDo)}} key={ToDo}>{ ToDo }</li>
                 })}
        </li>
      );
